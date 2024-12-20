b5fbf02e7662d90061f8dcfb26839e40c0e5c250
shira <118650861+imane539@users.noreply.github.com>
2024-12-20 12:37:15 +0100

    Update ft_pipex.c

--- ft_pipex.c
+++ ft_pipex.c
@@ -11,32 +11,60 @@ int main(int ac , char **av)
     // char *cmd1,*cmd2;
     int pipefd[2];
     int fd0,fd1;
+    int id,id1;
 /* ******************** create a pipe ***************************** */
     if(pipe(pipefd) == -1)
        return error("failed to create a pipe :( ");
 
 
-/* ****************** open the file1 to read *************************** */
-    fd0=open(av[1],O_RDONLY | O_CREAT); 
+/* ****************** open the file1 to read *************************** */ 
     if(access(av[1],R_OK) == -1)
        return error("you can't read from this file:( ");
 
     else
     {
        fd0=open(av[1],O_RDONLY); 
-       ft_printf("%d",fd0);
+       dup2(fd0,STDIN_FILENO);
     }
+    id = fork();  
+    if(id == -1)
+    return error("failed to create a child :( "); 
+        //child1
+    if(id == 0)
+    {
         
-
-    
-
-
+    }
+    else {
 /* ****************** open the file1 to write *************************** */
     
     if(access(av[4],W_OK) == -1)
        return error("you can't write in this file:( ");
 
     else
-    fd1=open(av[4],O_RDONLY);
+    {
+        fd1=open(av[4],O_WRONLY);
+        dup2(fd1,STDOUT_FILENO);
+    }
+    
+
+        
+        id1 = fork();
+        if(id1 == -1)
+             return error("failed to create a child 2:( "); 
+        //child2
+
+        if(id == 0)
+        {
+
+        }
+        //parent
+        else{
+
+        }
+    }
+    
+
+
+
 
 }
