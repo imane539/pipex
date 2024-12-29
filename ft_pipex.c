b10f551646d983463e648c82b270ce14c38bb7b0
shira <118650861+imane539@users.noreply.github.com>
2024-12-29 13:14:54 +0000

    ft_execve_fun

--- ft_pipex.c
+++ ft_pipex.c
@@ -1,29 +1,34 @@
 #include "ft_pipex.h"
-
+int ft_execeve(char *cmd,char **envp)
+{
+    char **argv;
+    size_t size;
+    char *path;
+    char *tes;
+    
+    tes=ft_strdup("/usr/bin/");
+    argv=ft_split(cmd,' ');
+    size = ft_strlen("/usr/bin/") + ft_strlen(argv[0]) + 1;
+    path = ft_strlcat(tes,argv[0],size);
+    if(execve(path,argv,envp) == -1)
+        return error("failed to exec :( "); 
+    return 0;
+}
 int main(int ac , char **av,char **envp)
 {
     if(ac != 5)
     return 0;
-    // char *cmd1;
     int pipefd[2];
     int fd0,fd1;
     int id,id1;
-    char **argv;
-    size_t size;
-     char *path;
-    char *tes;
     
-    tes=ft_strdup("/usr/bin/");
     if(pipe(pipefd) == -1)
        return error("failed to create a pipe :( ");
-
     if((fd0 = fdesc('r',av[1]) ) == 1)
         return 1;
-     //create new child
     id = fork();  
     if(id == -1)
     return error("failed to create a child :( "); 
-        //child1
        if(id == 0)
          {
             if(dup2(fd0,STDIN_FILENO) == -1)
@@ -31,38 +36,27 @@ int main(int ac , char **av,char **envp)
             close(fd0);
             if(dup2(pipefd[1],STDOUT_FILENO) == -1)
                 return error("failed to duplicate pipefd1:(");
-                        argv=ft_split(av[2],' ');
-                        size = ft_strlen("/usr/bin/") + ft_strlen(argv[0]) + 1;
-                        path = ft_strlcat(tes,argv[0],size);
-                        if(execve(path,argv,envp) == -1)
-                            return error("failed to exec :( "); 
+            close(pipefd[0]);
+            close(pipefd[1]);
+                        return ft_execeve(av[2],envp);
         }   
  else
  {
-/* ****************** open the file1 to write *************************** */
         close(pipefd[1]);
-
     if((fd1 = fdesc('w',av[4])) == 1)
             return 1;
     id1 = fork();
     if(id1 == -1)
              return error("failed to create a child 2:( "); 
-    //child2
     if(id1 == 0)
     { 
-         if(dup2(pipefd[0],STDIN_FILENO) == -1)
-                return error("failed to duplicate pipefd0:(");
-      
-             if(dup2(fd1,STDOUT_FILENO) == -1)
-                    return error("failed to duplicate fd1:(");
-                    //  close(pipefd[0]);
-                        argv=ft_split(av[3],' ');
-                        size = ft_strlen("/usr/bin/") + ft_strlen(argv[0]) + 1;
-                        path = ft_strlcat(tes,argv[0],size);
-                       
-                        if(execve(path,argv,envp) == -1)
-                            return error("failed to exec :( "); 
-        
+        if(dup2(pipefd[0],STDIN_FILENO) == -1)
+            return error("failed to duplicate pipefd0:(");
+        close(pipefd[0]);
+        if(dup2(fd1,STDOUT_FILENO) == -1)
+            return error("failed to duplicate fd1:(");
+        close(fd1);
+        return ft_execeve(av[3],envp);
     }
   
    
