ff93895e3ae71396c7489e67ae81ca5eafa258f6
shira <118650861+imane539@users.noreply.github.com>
2025-01-02 14:02:01 +0000

    waitfixed

--- ft_pipex.c
+++ ft_pipex.c
@@ -26,33 +26,21 @@ int	main(int ac, char **av, char **envp)
 	if (id == -1)
 		exit(error("failed to create a child :( "));
 	if (id == 0)
-	{
-		printf("1\t%d\n",getpid());
 		exit(child(fd0, pipefd[1], av[2], envp, pipefd[0]));
-	}
 	else
 	{
+	close( pipefd[1]);
 		if ((fd1 = fdesc('w', av[4])) == 1)
 			exit (1);
 		id1 = fork();
 		if (id1 == -1)
 			exit (error("failed to create a child 2:( "));
 		if (id1 == 0)
-		{
-			printf("2\t%d\n",getpid());
 			exit(child(pipefd[0], fd1, av[3], envp, pipefd[1]));
-		}
 		else{
-			printf("3\t%d\n",getpid());
-			printf("pid %d\n", waitpid(id,NULL,WNOHANG));
-			printf("pid %d\n", waitpid(id1,NULL,WNOHANG));
+			close(pipefd[0]);
+			printf("pid %d\n",waitpid(id, NULL, 0));
+			printf("pid %d\n", waitpid(id1, NULL, 0));
 		}
 	}
-	
-	// printf("pid %d\n", waitpid(id,NULL,WNOHANG));
-	// printf("pid %d\n", waitpid(id1,NULL,WNOHANG));
-	// printf("%d",wait(NULL));
-
-	// wait(NULL);
-
 }
